c558103a4c1cd07e0b729c30338cfb75963f691a
M00NH0RS3 <matheusmaciel.trampas@gmail.com>
2023-08-23 17:31:31 -0300

    .

--- main.py
+++ main.py
@@ -174,6 +174,6 @@ def sair():
     return redirect('/')
 
 if __name__ in '__main__':
-    # app.run(debug=1)
-    app.run(debug=0)
+    app.run(debug=1)
+    # app.run(debug=0)
     # socketio.run(app, host='192.168.0.58')
